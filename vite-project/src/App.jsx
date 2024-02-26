26a5538b6a9b0cc646b3ee26461abc365a781ba1
sina toosi <haami.toosi@gmail.com>
2024-02-27 02:23:24 +0330

    first commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+/* eslint-disable react/jsx-key */
+import TextItem from "./Components/TextItem";
+import { sculptureList } from "./"
 function App() {
-  const [count, setCount] = useState(0)
+const myArrey =[{name:"text1", age:20}, {name:"text2", age:30}]
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+    <TextItem title=" TextItem1"/>
+    {myArrey.map((i) =>{return (<div>{i.name}</div>)})}
+    </div>
   )
 }
-
-export default App
+  
+export default App;
---
+++ vite-project/src/Components/TextItem.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable react/prop-types */
+
+    const TextItem = ({ title }) => {
+        return <h2> {title} </h2>
+    }
+
+export default TextItem
